32cf3ff54c7b0a4896779b36afaf1045adf92a16
Patricio Melo <pmelor@stod.cl>
2023-09-24 17:18:38 -0300

    APP

---
+++ APP-Empresarial/EVA1-U1/src/main/java/com/mycompany/eva1/u1/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package com.mycompany.eva1.u1.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
